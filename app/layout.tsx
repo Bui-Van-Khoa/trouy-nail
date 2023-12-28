9696cefbabbcc70cac4f87b5ef3aa2f11528bf71
HWG\khoabui <khoabui@hayworkglobal.com>
2023-12-28 16:36:52 +0700

    build: implement header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,40 @@
-import { GeistSans } from 'geist/font/sans'
-import './globals.css'
-import Header from '@/components/Header/Header'
-import { Providers } from '@/lib/providers'
+import { GeistSans } from 'geist/font/sans';
+import './globals.css';
+import { Providers } from '@/lib/providers';
+import dynamic from 'next/dynamic';
+// import Header from '@/components/header/Header';
 
+const Header = dynamic(() => import('@/components/header/Header'), {
+  ssr: false,
+});
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000'
+  : 'http://localhost:3000';
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: 'Next.js and Supabase Starter Kit',
   description: 'The fastest way to build apps with Next.js and Supabase',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-		<Providers>
-			<html lang="en" className={GeistSans.className}>
-				<body className="bg-background text-foreground">
-					<Header />
-					<main className="min-h-screen flex flex-col items-center">
-						{children}
-					</main>
-				</body>
-			</html>
-			</Providers>
-  )
+    <Providers>
+      <html lang="en" className={GeistSans.className}>
+        <body className="bg-background text-foreground px-16 tablet:px-10 pt-5">
+          <Header />
+          <main className="min-h-screen flex flex-col items-center">
+            {children}
+          </main>
+        </body>
+      </html>
+    </Providers>
+  );
 }
 
 
